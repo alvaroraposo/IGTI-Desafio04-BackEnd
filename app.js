85d103830598b18a767080077476f30eb6148409
alvaroraposo <alvaroraposo@yahoo.com.br>
2020-09-05 22:02:49 +0100

    cors

--- app.js
+++ app.js
@@ -26,7 +26,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(
   cors({
-    origin: 'https://igtibackend.herokuapp.com/',
+    origin: 'https://igtifrontend.herokuapp.com/',
   })
 );
 app.use(gradeRouter);
